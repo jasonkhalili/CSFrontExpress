78f647096ae5596c652831b8821db81cab815345
Mitchell Valine <mitchellvaline@gmail.com>
2015-06-01 15:49:36 -0700

    bower functionality added, scss compile added to gulpfile, dependencies updated

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 
 gulp.task('js', function() {
     browserify('./public/javascripts/src/app.jsx')
@@ -13,10 +14,17 @@ gulp.task('js', function() {
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('public/javascripts/build/'));
-});
+})
+
+gulp.task('sass', function() {
+    gulp.src('public/stylesheets/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/stylesheets/css'));
+})
 
 gulp.task('watch', function() {
-    gulp.watch("public/javascripts/src/**/*.jsx", ["js"])
-});
+    gulp.watch("public/javascripts/src/**/*.jsx", ["js"]);
+    gulp.watch("public/stylesheets/scss/*.scss", ["sass"]);
+})
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', ['js', 'sass', 'watch']);
