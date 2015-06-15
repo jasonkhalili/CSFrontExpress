35153a566d965e13069b1bf2ae8b17f8e92a4a15
Mitchell Valine <mitchellvaline@gmail.com>
2015-06-14 23:33:12 -0700

    added loadInventoryFromServer to InventoryBox.jsx

--- public/javascripts/src/InventoryBox.jsx
+++ public/javascripts/src/InventoryBox.jsx
@@ -1,17 +1,31 @@
 var React = require('../../libraries/react/react-with-addons.js');
 var $ = jQuery = require('../../libraries/jquery/dist/jquery');
 
-var SteamTradeOffers = require('steam-tradeoffers');
-
-var offers = new SteamTradeOffers();
+inventoryURL = 'http://steamcommunity.com/profiles/'+user_id+'/inventory/json/730/2';
 
 module.exports =  React.createClass({
+    loadInventoryFromServer: function() {
+        $.ajax({
+            url: inventoryURL,
+            dataType: 'json',
+            type: 'GET',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadInventoryFromServer();
+    },
     render: function() {
-        console.log(offers);
         return (
             <div className="inventoryBox">
-                <p>test</p>
+                <p>{this.state.data}</p>
             </div>
         );
     }
-})
+});
