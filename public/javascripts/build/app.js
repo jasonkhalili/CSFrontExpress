369d00e386af789a7a8ecd1e43a26c4598ce8a9f
Mitchell Valine <mitchellvaline@yahoo.com>
2015-06-30 23:02:50 -0700

    app.js rebuild

--- public/javascripts/build/app.js
+++ public/javascripts/build/app.js
@@ -34137,3 +34137,43 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 },{}]},{},[1]);
+ = new Affix(this, options)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
+
+  var old = $.fn.affix
+
+  $.fn.affix             = Plugin
+  $.fn.affix.Constructor = Affix
+
+
+  // AFFIX NO CONFLICT
+  // =================
+
+  $.fn.affix.noConflict = function () {
+    $.fn.affix = old
+    return this
+  }
+
+
+  // AFFIX DATA-API
+  // ==============
+
+  $(window).on('load', function () {
+    $('[data-spy="affix"]').each(function () {
+      var $spy = $(this)
+      var data = $spy.data()
+
+      data.offset = data.offset || {}
+
+      if (data.offsetBottom != null) data.offset.bottom = data.offsetBottom
+      if (data.offsetTop    != null) data.offset.top    = data.offsetTop
+
+      Plugin.call($spy, data)
+    })
+  })
+
+}(jQuery);
+
+},{}]},{},[1]);
